5c89b37d49ee3eed6f520dd26e0409b65af6993d
Valentin Popov <valentin@popov.link>
2024-01-11 20:29:23 +0400

    Initial CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Create a mirror of a repository
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The path to save the repository to
+    #[arg(short, long)]
+    path: String,
+
+    /// The URL of the repository to clone
+    #[arg(short, long)]
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
